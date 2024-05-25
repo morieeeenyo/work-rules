567c5208f6e376b80312a33335d889838e1c4d9b
morieeeenyo <uint2435@gmail.com>
2024-05-26 07:35:50 +0900

    chore: 不要なconsole.log削除

--- app/workrules/hooks/useAnswerRetrospective.ts
+++ app/workrules/hooks/useAnswerRetrospective.ts
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 export const useAnswerRetrospective = () => {
   const onSubmitAnswer = async (selectedRowIds: string[]) => {
-    console.log(selectedRowIds)
     const response = await axios.post('/api/workrules/retrospective', {
       selectedRowIds,
     })
