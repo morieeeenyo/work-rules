34525b06fad2dd72861b4f2f8006913987041d51
morieeeenyo <uint2435@gmail.com>
2024-06-02 07:19:55 +0900

    feat: pageの骨格を作成

--- app/workrules/retrospectives/page.tsx
+++ app/workrules/retrospectives/page.tsx
@@ -1,9 +1,50 @@
 'use client'
 
+import { Navigation } from '@mui/icons-material'
+import { Fab, Grid, Table, Typography } from '@mui/material'
+
 export default function RetrospectiveAnswerList() {
   return (
-    <div>
-      <h1>RetrospectiveAnswerList</h1>
-    </div>
+    <Grid
+      container
+      justifyContent='center'
+      alignItems='center'
+      flexDirection='column'
+      margin='0 auto'
+      height='100%'
+      spacing={4}
+    >
+      <Grid item container justifyContent='space-between' width='960px'>
+        <Grid item>
+          <Typography variant='h4' fontWeight='bold'>
+            回答結果一覧
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Table />
+      </Grid>
+      <Grid
+        item
+        container
+        justifyContent='end'
+        alignItems='center'
+        width='960px'
+        position='fixed'
+        bottom={20}
+      >
+        <Grid item pr='2rem'>
+          <Fab
+            variant='extended'
+            onClick={() => {
+              window.scrollTo({ top: 0, behavior: 'smooth' })
+            }}
+          >
+            <Navigation sx={{ mr: 1 }} />
+            ページTOPへ
+          </Fab>
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
